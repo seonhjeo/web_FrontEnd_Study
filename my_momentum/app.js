a8fcefe4e0b353360ef891783cda1e533ba4c211
seonhjeo <42938826+seonhjeo@users.noreply.github.com>
2022-10-02 23:43:40 +0900

    practice with css

--- my_momentum/app.js
+++ my_momentum/app.js
@@ -1,27 +1,12 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleh1Click() {
-    console.log("title was clicked!");
-}
-
-function handleMouseEnter() {
-    h1.style.color = "blue";
-}
-function handleMouseLeave() {
-    h1.style.color = "black";
-}
-function handleWindowResize()
-{
-    document.body.style.backgroundColor = "tomato";
-}
-function handeWindowCopy()
-{
-    alert("copier!");
+    const activeClass = "active";
+    if (h1.classList.contains(activeClass)) {
+        h1.classList.remove(activeClass);
+    } else {
+        h1.classList.add(activeClass);
+    }   
 }
 
 h1.addEventListener("click", handleh1Click);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handeWindowCopy);
